f4148338e11df46de6d96bccf743453e86746a38
Rainer <rainermr89@hotmail.com>
2022-03-09 12:43:03 -0300

    Referencias de DLL

--- ByteBank.SistemaInterno/Program.cs
+++ ByteBank.SistemaInterno/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,14 @@ using System.Threading.Tasks;
 
 namespace ByteBank.SistemaInterno
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            ContaCorrente conta = new ContaCorrente(154, 785412);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
         }
     }
 }
